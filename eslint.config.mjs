d5d3f4c22bb1e2152ad8ef3dd44050f46b29371f
homeword-wind <80752627+homeword-wind@users.noreply.github.com>
2025-01-03 10:15:10 +0800

    feat: improve rollup packaging functionality

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,34 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,ts,jsx,mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.browser } },
+  {
+    ignores: ['node_modules/!**']
+  },
+  {
+    // 9.0.0 版本中移除了--ext，文件拓展名在此处配置
+    files: ['**/*.{js,ts,jsx,mjs,cjs,ts}']
+  },
+  {
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended
-]
+  ...tseslint.configs.recommended,
+  {
+    // 规则配置
+    rules: {
+      'prefer-const': 'off',
+      'no-case-declarations': 'off',
+      'no-constant-condition': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-unused-vars': 'off'
+    }
+  }
+];
---
+++ scripts/rollup/utils.js
@@ -0,0 +1,32 @@
+import path from 'path'
+import fs from 'fs'
+
+import ts from 'rollup-plugin-typescript2'
+import cjs from '@rollup/plugin-commonjs'
+
+// __dirname 在 ESM 模式下使用使用会报错
+// polyfill
+import { fileURLToPath } from 'url'
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+const pkgPath = path.resolve(__dirname, '../../packages')
+const distPath = path.resolve(__dirname, '../../dist/node_modules')
+
+export function resolvePkgPath(pkgName, isDist) {
+  if (isDist) {
+    return `${distPath}/${pkgName}`
+  }
+
+  return `${pkgPath}/${pkgName}`
+}
+
+export function getPackageJSON(pkgName) {
+  // 包路径
+  const path = `${resolvePkgPath(pkgName)}/package.json`
+  const str = fs.readFileSync(path, { encoding: 'utf-8' })
+  return JSON.parse(str)
+}
+
+export function getBaseRollupPlugins({ typescript = {} } = {}) {
+  return [cjs(), ts()]
+}
